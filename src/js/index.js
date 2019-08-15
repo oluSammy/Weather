3e5580840badee9b481fcec6daca4279fb4afaf4
oluSammy <olumorinsammy@gmail.com>
2019-08-15 02:06:42 +0100

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import x from './test';
+const y = 'allright';
+console.log(`i imported ${x} frm another module from ok me, errthing is ${y}`); 
+// fetch('https://api.openweathermap.org/data/2.5/forecast?q=lagos&appid=67d2ed2bdf46af7df4a0695665f86c8f')
+// .then(result => {
+//     // console.log(result);
+//     return result.json();
+// })
+// .then(data=>{
+//     console.log(data);
+// })
+// .catch(error =>{
+//     console.log(error);
+// })
